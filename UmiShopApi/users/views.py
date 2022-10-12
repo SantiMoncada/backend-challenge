e3cc918acc2b61c8f192286aadfba33f5e6234d8
Santiago Moncada <90731161+SantiMoncada@users.noreply.github.com>
2022-10-12 11:13:31 +0200

    added send email feature

--- UmiShopApi/users/views.py
+++ UmiShopApi/users/views.py
@@ -1,6 +1,3 @@
-from pickle import FALSE
-from django.shortcuts import render
-
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
