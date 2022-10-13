72195ad671c4bb920f13447e4d7a1c5ffe6cfc2e
Santiago Moncada <90731161+SantiMoncada@users.noreply.github.com>
2022-10-13 19:24:16 +0200

    added api to urls

--- UmiShopApi/UmiShopApi/urls.py
+++ UmiShopApi/UmiShopApi/urls.py
@@ -23,6 +23,6 @@ schema_view = get_schema_view(
 urlpatterns = [
     path('', schema_view.with_ui('swagger', cache_timeout=0),
          name='schema-swagger-ui'),
-    path('', include('users.urls')),
-    path('', include('assistance.urls'))
+    path('api/', include('users.urls')),
+    path('api/', include('assistance.urls'))
 ]
--- UmiShopApi/test.sh
+++ UmiShopApi/test.sh
@@ -1,7 +1,13 @@
-curl http://127.0.0.1:8000/user -H 'Content-Type: application/json'
-curl http://127.0.0.1:8000/user/4 -H 'Content-Type: application/json'
+# Get all users
+curl http://127.0.0.1:8000/api/user -H 'Content-Type: application/json'
 
-curl -X POST http://127.0.0.1:8000/user -H 'Content-Type: application/json' \
+# get a user
+curl http://127.0.0.1:8000/api/user/1 -H 'Content-Type: application/json'
+
+# create a ne wuser
+curl -X POST http://127.0.0.1:8000/api/user -H 'Content-Type: application/json' \
   -d '{"email": "mike@hey.com", "phone": 444444444, "name": "Michael"}'
-curl -X POST http://127.0.0.1:8000/assistance -H 'Content-Type: application/json' \
+
+#Send assistance to a customer
+curl -X POST http://127.0.0.1:8000/api/assistance -H 'Content-Type: application/json' \
   -d '{"topic": "sales", "email": "Santi@hey.com"}'
