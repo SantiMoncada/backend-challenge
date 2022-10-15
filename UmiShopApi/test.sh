0e7ee333149b5eb9ac7fd2e05f517e4266cfe83e
Santiago Moncada <90731161+SantiMoncada@users.noreply.github.com>
2022-10-15 15:25:01 +0200

    added crud to issues

--- UmiShopApi/test.sh
+++ UmiShopApi/test.sh
@@ -4,7 +4,7 @@ curl http://127.0.0.1:8000/api/user -H 'Content-Type: application/json'
 # get a user
 curl http://127.0.0.1:8000/api/user/1 -H 'Content-Type: application/json'
 
-# create a ne wuser
+# create a new user
 curl -X POST http://127.0.0.1:8000/api/user -H 'Content-Type: application/json' \
   -d '{"email": "mike@hey.com", "phone": 444444444, "name": "Michael"}'
 
