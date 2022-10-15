83e0d4b89199d16fd245bf59371b96409abbbf99
Santiago Moncada <90731161+SantiMoncada@users.noreply.github.com>
2022-10-15 18:55:47 +0200

    added swagger documentation

--- UmiShopApi/assistance/views.py
+++ UmiShopApi/assistance/views.py
@@ -1,3 +1,4 @@
+import email
 from django.http import JsonResponse
 from rest_framework.parsers import JSONParser
 
@@ -7,10 +8,30 @@ from assistance.models import Assistance
 from assistance.serializer import AssistanceSerializer
 from .taks import contact_product
 
+from drf_yasg.utils import swagger_auto_schema
+from drf_yasg import openapi
 
+
+@swagger_auto_schema(method='GET',
+                     operation_description="Get all the issues created on the DB")
+@swagger_auto_schema(method='POST',
+                     request_body=openapi.Schema(
+                         type=openapi.TYPE_OBJECT,
+                         required=['email', 'topic', 'address'],
+                         properties={
+                             'email': openapi.Schema(type=openapi.TYPE_STRING),
+                             'topic': openapi.Schema(type=openapi.TYPE_STRING),
+                             'address': openapi.Schema(type=openapi.TYPE_STRING),
+                         },
+                         default={
+                             'email': 'person@example.com',
+                             'topic': 'Sales',
+                             'address': "Madrid, Madrid",
+                         }
+                     ),
+                     operation_description='Sends a new notification to the product team and saves it on the DB')
 @api_view(['GET', 'POST'])
 def assistance(request):
-
     if request.method == 'GET':
         assistances = Assistance.objects.all()
         serializer = AssistanceSerializer(assistances, many=True)
@@ -29,6 +50,10 @@ def assistance(request):
     return JsonResponse({'errorMsg': 'Bad request'}, status=400)
 
 
+@swagger_auto_schema(method='GET',
+                     operation_description="Get a specific issue by ID")
+@swagger_auto_schema(method='DELETE',
+                     operation_description="Delete a spesific issue by ID")
 @api_view(['GET', 'DELETE'])
 def assistance_details(request, pk):
     try:
