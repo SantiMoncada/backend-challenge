60c8b631d63051d2a0530eae7a06454c628ec8dd
SantiMoncada <santiago.m.a@hotmail.com>
2022-10-16 20:36:29 +0200

    fixed typo on swagger

--- UmiShopApi/UmiShopApi/urls.py
+++ UmiShopApi/UmiShopApi/urls.py
@@ -9,12 +9,9 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
-        title="Snippets API",
+        title="UmiShop API",
         default_version='v1',
-        description="Test description",
-        terms_of_service="https://www.google.com/policies/terms/",
-        contact=openapi.Contact(email="contact@snippets.local"),
-        license=openapi.License(name="BSD License"),
+        description="Documentation for the Backend Challenge"
     ),
     public=True,
     permission_classes=[permissions.AllowAny],
